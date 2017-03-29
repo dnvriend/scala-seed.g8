bcebdb8a1c67884e2a637c98e1c8e69eb74e3407
Dennis Vriend <dnvriend@gmail.com>
2017-03-29 06:51:43 +0100

    Update TestSpec.scala

--- src/main/g8/src/test/scala/$package$/TestSpec.scala
+++ src/main/g8/src/test/scala/$package$/TestSpec.scala
@@ -1,6 +1,13 @@
 package $package$
 
-import org.scalatest.{FlatSpec, Matchers}
-import org.typelevel.scalatest.{ValidationMatchers, DisjunctionMatchers}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{FlatSpec, Matchers, OptionValues}
+import org.typelevel.scalatest.{DisjunctionMatchers, ValidationMatchers}
 
-abstract class TestSpec extends FlatSpec with Matchers with ValidationMatchers with DisjunctionMatchers
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+abstract class TestSpec extends FlatSpec with Matchers with ValidationMatchers with DisjunctionMatchers with ScalaFutures with OptionValues {
+  implicit val pc: PatienceConfig = PatienceConfig(timeout = 60.minutes, interval = 300.millis)
+  implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+}
